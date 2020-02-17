2c00826f1af822fd4cf8892689dc6ef62ef55ddf
Schlenges <meike_hankewicz@yahoo.de>
2020-02-17 21:36:05 +0100

    Add cors and body-parser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 mongoose
   .connect(config.MONGO_URL, {
@@ -9,5 +11,8 @@ mongoose
     useUnifiedTopology: true
   })
   .catch((error) => console.log('error connecting to MongoDB:', error.message))
+
+app.use(cors())
+app.use(bodyParser.json())
 
 module.exports = app
