7efc44d0bd94615b9c2941f68d30f243c39c22af
Schlenges <meike_hankewicz@yahoo.de>
2020-02-26 23:16:19 +0100

    fixed user api tests

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -20,113 +20,117 @@ beforeEach(async () => {
   await user.save()
 })
 
-test('Valid user gets succesfully created', async () => {
-  const usersAtStart = await usersInDb()
+describe('Adding Users', () => {
 
-  const newUser = {
-    username: 'Luigi',
-    password: 'Yoshi123',
-  }
+  test('Valid user gets succesfully created', async () => {
+    const usersAtStart = await usersInDb()
+  
+    const newUser = {
+      username: 'Luigi',
+      password: 'Yoshi123',
+    }
+  
+    await api
+      .post(url)
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const usersAtEnd = await usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
+  
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+  
+  test('Creation fails when username is already taken', async () => {
+    const usersAtStart = await usersInDb()
+  
+    const newUser = {
+      username: 'Mario', 
+      password: '1234'
+    }
+  
+    await api
+      .post(url)
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  
+    // expect(result.body.error).toContain('`username` to be unique')
+  
+    const usersAtEnd = await usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length)
+  })
+  
+  test('Creation fails if username or password is missing', async () => {
+    const usersAtStart = await usersInDb()
+  
+    const missingUsername = {
+      password: "Yoshi1234"
+    }
+  
+    const missingPassword = {
+      username: "Lugi"
+    }
+  
+    await api
+      .post(url)
+      .send(missingUsername)
+      .expect(400)
+      .then((res) => expect(res.body.error).toContain("`username` is required"))
+  
+    await api
+      .post(url)
+      .send(missingPassword)
+      .expect(400)
+      .then((res) => expect(res.body.error).toContain("password is required"))
+  
+    const usersAtEnd = await usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length)
+  })
+  
+  test('Creation fails if username or password is shorter than 3 characters', async () => {
+    const usersAtStart = await usersInDb()
+  
+    const shortUsername = {
+      username: "Lu",
+      password: "123"
+    }
+  
+    const shortPassword = {
+      username: "Luigi",
+      password: "1"
+    }
+  
+    await api
+      .post(url)
+      .send(shortUsername)
+      .expect(400)
+      .then((res) => expect(res.body.error).toContain("is shorter than the minimum allowed"))
+  
+    await api
+      .post(url)
+      .send(shortPassword)
+      .expect(400)
+      .then((res) => expect(res.body.error).toContain("password must be at least 3 characters"))
+  
+    const usersAtEnd = await usersInDb()
+    expect(usersAtEnd.length).toBe(usersAtStart.length)
+  })
+  
+  test('Created user has skills array', async () => {
+    const newUser = {
+      username: "Luigi",
+      password: "Yoshi123"
+    }
+  
+    await api
+      .post(url)
+      .send(newUser)
+      .then((res) => expect(res.body.skills).toEqual([]))
+  })
 
-  await api
-    .post(url)
-    .send(newUser)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const usersAtEnd = await usersInDb()
-  expect(usersAtEnd.length).toBe(usersAtStart.length + 1)
-
-  const usernames = usersAtEnd.map(u => u.username)
-  expect(usernames).toContain(newUser.username)
-})
-
-test('Creation fails when username is already taken', async () => {
-  const usersAtStart = await usersInDb()
-
-  const newUser = {
-    username: 'Mario', 
-    password: '1234'
-  }
-
-  await api
-    .post(url)
-    .send(newUser)
-    .expect(400)
-    .expect('Content-Type', /application\/json/)
-
-  // expect(result.body.error).toContain('`username` to be unique')
-
-  const usersAtEnd = await usersInDb()
-  expect(usersAtEnd.length).toBe(usersAtStart.length)
-})
-
-test('Creation fails if username or password is missing', async () => {
-  const usersAtStart = await usersInDb()
-
-  const missingUsername = {
-    password: "Yoshi1234"
-  }
-
-  const missingPassword = {
-    username: "Lugi"
-  }
-
-  await api
-    .post(url)
-    .send(missingUsername)
-    .expect(400)
-    .expect((res) => res.body.error.includes("username is required"))
-
-  await api
-    .post(url)
-    .send(missingPassword)
-    .expect(400)
-    .expect((res) => res.body.error.includes("password is required"))
-
-  const usersAtEnd = await usersInDb()
-  expect(usersAtEnd.length).toBe(usersAtStart.length)
-})
-
-test('Creation fails if username or password is shorter than 3 characters', async () => {
-  const usersAtStart = await usersInDb()
-
-  const shortUsername = {
-    username: "Lu",
-    password: "123"
-  }
-
-  const shortPassword = {
-    username: "Luigi",
-    password: "1"
-  }
-
-  await api
-    .post(url)
-    .send(shortUsername)
-    .expect(400)
-    .expect((res) => res.body.error.includes("is shorter than the minimum allowed"))
-
-  await api
-    .post(url)
-    .send(shortPassword)
-    .expect(400)
-    .expect((res) => res.body.error.includes("password must be at least 3 characters"))
-
-  const usersAtEnd = await usersInDb()
-  expect(usersAtEnd.length).toBe(usersAtStart.length)
-})
-
-test('Created user has skills array', async () => {
-  const newUser = {
-    username: "Luigi",
-    password: "Yoshi123"
-  }
-
-  await api
-    .post(url)
-    .send(newUser)
-    .expect((res) => res.body.skills = [])
 })
 
 afterAll(() => {
