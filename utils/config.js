fe478157b71729925acbd5f85cb8e9c3a36dcb2d
Schlenges <meike_hankewicz@yahoo.de>
2020-03-13 17:00:11 +0100

    Enhancement: Add cross-env, set environments

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGO_URL = process.env.MONGO_URL
+let MONGO_URL = process.env.MONGO_URL
+
+if(process.env.NODE_ENV === 'test'){
+  MONGO_URL = process.env.TEST_MONGO_URL
+}
 
 module.exports = {
   MONGO_URL,
